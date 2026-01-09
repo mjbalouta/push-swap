558fde00f0fadc45bd25653cd7bf91c38d0448dd
Maria João Balouta <165146704+mjbalouta@users.noreply.github.com>
2026-01-09 16:35:04 +0000

    Fix include path for push_swap.h

--- srcs/utils/bit_manipulation.c
+++ srcs/utils/bit_manipulation.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
+#include "push_swap.h"
 
 int	fill_binary_array(t_stack *stack_a)
 {
