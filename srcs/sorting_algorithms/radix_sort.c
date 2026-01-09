c78cde6b27337294628c5a96e4666cf410461aa3
Maria João Balouta <165146704+mjbalouta@users.noreply.github.com>
2026-01-09 16:34:40 +0000

    Fix include path for push_swap.h

--- srcs/sorting_algorithms/radix_sort.c
+++ srcs/sorting_algorithms/radix_sort.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
+#include "push_swap.h"
 
 int	fill_index(t_stack *stack_a)
 {
