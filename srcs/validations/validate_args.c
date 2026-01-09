3e0d3e07404d31e1b13614ea047c6b25c238ad0d
Maria João Balouta <165146704+mjbalouta@users.noreply.github.com>
2026-01-09 16:35:47 +0000

    Fix include path for push_swap.h

--- srcs/validations/validate_args.c
+++ srcs/validations/validate_args.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
+#include "push_swap.h"
 
 int	detect_duplicates(t_stack *stack_a)
 {
